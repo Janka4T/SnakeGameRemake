79e0e45eaa1cbd43a63d6464dc11083be21914ed
Jānis Tulišs <janistuliss3@gmail.com>
2020-06-05 16:32:43 +0300

    V.2

--- Game.cs
+++ Game.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,7 @@ namespace SnakeGameRemake
     public partial class Game : Form
     {
         GameZone gameZone = null;
+        Snake snake = null;
         public Game()
         {
             InitializeComponent();
@@ -21,8 +23,15 @@ namespace SnakeGameRemake
 
         private void InitializeGame()
         {
+            this.Width = 500;
+            this.Height = 500;
+
             gameZone = new GameZone();
+            gameZone.Location = new Point(50, 50);
             this.Controls.Add(gameZone);
+
+            snake = new Snake();
+            
         }
     }
 }
---
+++ Snake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace SnakeGameRemake
+{
+    class Snake
+    {
+        private PictureBox snakePixel;
+        private List<PictureBox> snakePixels = new List<PictureBox>();
+
+        public Snake()
+        {
+            snakePixel = new PictureBox()
+            {
+                BackColor = Color.Black,
+                Width = 20,
+                Height = 20,
+                Left = 200,
+                Top = 200
+            };
+            snakePixels.Add(snakePixel);
+            
+        }
+    }
+}
